4d73632496b361618c590504b149e62b476fd575
Olamide Olayemi <olamid15@gmail.com>
2025-06-28 23:49:28 +0100

    feat: Implement account retrieval and banking features with Plaid integration

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,28 @@
 import HeaderBox from '@/components/HeaderBox';
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getAccount, getAccounts } from '@/lib/actions/banks.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react';
 
-const Home = async () => {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
 	const loggedIn = await getLoggedInUser();
+
+	const accounts = await getAccounts({
+		userId: loggedIn.$id,
+	});
+
+	if (!accounts) return;
+
+	const accountsData = accounts?.data;
+	const appwriteItemId = (id as string) || accounts?.data[0]?.appwriteItemId;
+
+	const account = await getAccount({ appwriteItemId });
+
+	console.log({
+		accountsData,
+		account,
+	});
 
 	return (
 		<section className='home'>
@@ -18,9 +35,9 @@ const Home = async () => {
 						subtext='Access and manage your account and transactions effectively.'
 					/>
 					<TotalBalanceBox
-						accounts={[]}
-						totalBanks={1}
-						totalCurrentBalance={1250.35}
+						accounts={accountsData}
+						totalBanks={accounts?.totalBanks}
+						totalCurrentBalance={accounts?.totalCurrentBalance}
 					/>
 				</header>
 				RECENT TRANSACTIONS
@@ -28,8 +45,8 @@ const Home = async () => {
 
 			<RightSidebar
 				user={loggedIn}
-				transactions={[]}
-				banks={[{ currentBalance: 125.32 }, { currentBalance: 525.32 }]}
+				transactions={accounts?.transactions}
+				banks={accountsData?.slice(0, 2)}
 			/>
 		</section>
 	);
--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -9,7 +9,7 @@ const AnimatedCounter = ({ amount }: { amount: number }) => {
     //   duration={2.75}
       decimals={2}
       decimal=","
-      prefix="₦"
+      prefix="$"
       end={amount} />
     </div>
   );
